deb1af72e8930f27f62a41af049d3c02ee0ec69a
Your Name <you@example.com>
2020-07-14 12:28:29 -0300

    clase 14/07

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,10 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Carrizo");
+	//alert("Carrizo");//ESTO ES UN COMENTARIO DE LINEA
+
+	//continue("Desea continuar?");
+	prompt("ingrese su nombre");
 }
+
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,25 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato;
+	
+	prompt("Ingrese un dato");
+
+	alert("dato");
+	
+	//alert(prompt)("Ingrese un dato"));//NO
+	//var nobmbre;//undefined
+	//var nombreCompletoYApellidoDeLaMadre
+
+	var nombre;
+
+	nombre= "Santiago";//string
+	nombre=null;// null
+	nombre=12;// number->NaN, Infinity
+	nombre=true;// boolean
+	alert(nombre);
+
+	
+
 }
 
