129b6ca7c4ad94008ff121f4b1e8bc3abde3bd60
Your Name <you@example.com>
2020-08-05 12:25:54 -0300

    Clase 05/08

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,31 @@
 
 function mostrar()
 {
+ var contador = 0;
+ var tipodeProducto;
+
+
+ while(contador < 5) { 
+     contador++;
+
+     do { 
+
+        tipodeProducto = prompt("ingrese el tipo de producto");
+      } while (tipodeProducto != "barbijo";
+        
+        && tipodeProducto!="Jabon"
+        && tipodeProducto !="Alcohol");
+        
+        do { 
+            precio = prompt ("Ingrese un precio");
+            precio = parseInt(precio);
+        } while (precio <100 || precio > 300 || isNaN(precio));
+    }
+    
+
+
+
+
+
 
 }
